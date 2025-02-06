df64dcc89319bd5b96161c16262afc299191a688
Olawoyin <yustee2017@gmail.com>
2025-02-06 05:31:11 +0100

    Nabar brand can direct to homepage

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ServiceCard from '../Components/ServiceCard'
+import Card from '../Components/Card'
 
 const Services = () => {
   return (
     <div>
         <ServiceCard />
+        <Card />
     </div>
   )
 }
